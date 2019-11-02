7c9a9cb733404723666ec771d16f37e29e3adc5b
Bryan Alonso Valencia <alonsoval2.03@gmail.com>
2019-11-02 00:00:28 -0600

    docs: Se agrego el archivo README

--- Listas.py
+++ Listas.py
@@ -1,10 +1,3 @@
-# Implementación de una lista enlazada para resolver el problema:
-
-"""
-En una lista enlazada de números enteros, se desea añadir un nodo entre dos nodos consecutivos cuyos datos tienen
-	distinto signo;	el dato del nuevo nodo debe ser la diferencia, en valor absoluto, de los dos nodos.
-"""
-
 # Creamos la clase node
 class node:
     def __init__(self, data=None, next=None):
---
+++ README.md
@@ -0,0 +1,7 @@
+**Implementación de una lista simplememnte enlazada para 
+resolver el problema:**
+
+    En una lista enlazada de números enteros, se desea añadir 
+    un nodo entre dos nodos consecutivos cuyos datos tienen
+	distinto signo;	el dato del nuevo nodo debe ser la 
+	diferencia, en valor absoluto, de los dos nodos.
