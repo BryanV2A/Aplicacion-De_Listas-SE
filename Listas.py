677972d0612639d991fd8d0e0b4dd4c868ea1fdb
Bryan Alonso Valencia <alonsoval2.03@gmail.com>
2019-11-01 23:35:34 -0600

    feat: Se agregaron los metodos para listas simplememnte enlazadas.
    docs: Se respetaron los comentarios que estos tenian.
    
    test: Tambien se respetaron las pruebas que se habian echo y se agregaron.

--- Listas.py
+++ Listas.py
@@ -5,4 +5,65 @@ En una lista enlazada de números enteros, se desea añadir un nodo entre dos no
 	distinto signo;	el dato del nuevo nodo debe ser la diferencia, en valor absoluto, de los dos nodos.
 """
 
+# Creamos la clase node
+class node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
 class AplicacionDeListasEnlazadas:
+    def __init__(self):
+        self.head = None
+
+    # Método para agregar elementos en el frente de la linked list
+    def add_at_front(self, data):
+        self.head = node(data=data, next=self.head)
+
+    # Método para verificar si la estructura de datos esta vacia
+    def is_empty(self):
+        return self.head == None
+
+    # Método para agregar elementos al final de la linked list
+    def add_at_end(self, data):
+        if not self.head:
+            self.head = node(data=data)
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = node(data=data)
+
+    # Método para eliminar nodos
+    def delete_node(self, key):
+        curr = self.head
+        prev = None
+        while curr and curr.data != key:
+            prev = curr
+            curr = curr.next
+        if prev is None:
+            self.head = curr.next
+        elif curr:
+            prev.next = curr.next
+            curr.next = None
+
+    # Método para obtener el ultimo nodo
+    def get_last_node(self):
+        temp = self.head
+        while (temp.next is not None):
+            temp = temp.next
+        return temp.data
+
+    # Método para imprimir la lista de nodos
+    def print_list(self):
+        node = self.head
+        while node != None:
+            print(node.data, end=" => ")
+            node = node.next
+
+
+s = AplicacionDeListasEnlazadas()  # Instancia de la clase
+s.add_at_front(5)  # Agregamos un elemento al frente del nodo
+s.add_at_end(8)  # Agregamos un elemento al final del nodo
+s.add_at_front(9)  # Agregamos otro elemento al frente del nodo
+
+s.print_list()  # Imprimimos la lista de nodos
